5da269ce8e21d3f5c27ad8f0d52acffe353d9dcd
santosmarco <eu@marco.rio.br>
2021-08-17 16:15:54 -0300

    Fixed bug with RNPaper DatePicker on Android

--- App.tsx
+++ App.tsx
@@ -14,6 +14,36 @@ import { AuthNav, MainNav, SuperNav, ViewOnlyNav } from "./nav";
 import { LoadingScreen } from "./screens";
 import store from "./store";
 
+const isAndroid = require("react-native").Platform.OS === "android";
+// @ts-ignore
+const isHermesEnabled = !!global.HermesInternal;
+
+if (isHermesEnabled || isAndroid) {
+  require("@formatjs/intl-getcanonicallocales/polyfill");
+  require("@formatjs/intl-locale/polyfill");
+  require("@formatjs/intl-pluralrules/polyfill");
+  require("@formatjs/intl-pluralrules/locale-data/pt.js");
+  require("@formatjs/intl-displaynames/polyfill");
+  require("@formatjs/intl-displaynames/locale-data/pt.js");
+  require("@formatjs/intl-listformat/polyfill");
+  require("@formatjs/intl-listformat/locale-data/pt.js");
+  require("@formatjs/intl-numberformat/polyfill");
+  require("@formatjs/intl-numberformat/locale-data/pt.js");
+  require("@formatjs/intl-relativetimeformat/polyfill");
+  require("@formatjs/intl-relativetimeformat/locale-data/pt.js");
+  require("@formatjs/intl-datetimeformat/polyfill");
+  require("@formatjs/intl-datetimeformat/locale-data/pt.js");
+
+  require("@formatjs/intl-datetimeformat/add-golden-tz.js");
+
+  if ("__setDefaultTimeZone" in Intl.DateTimeFormat) {
+    // @ts-ignore
+    Intl.DateTimeFormat.__setDefaultTimeZone(
+      require("expo-localization").timezone
+    );
+  }
+}
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
